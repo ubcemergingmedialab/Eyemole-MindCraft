1a448950cffe8d91b4c4a1d47c5710e20152324e
Yana Pertels <ypertels@gmail.com>
2017-11-21 23:36:04 -0800

    dynamic textures on cube, 2 colour schemes based on peak frequency

--- Assets/Scripts/ChangeFaceColour.cs
+++ Assets/Scripts/ChangeFaceColour.cs
@@ -12,14 +12,18 @@ public class ChangeFaceColour : MonoBehaviour {
 	private Texture2D texture;
 	private Material material;
 	public int recordingTime = 5;
-	public int numFreqs = 60;
+	public int numFreqs = 40;
 	public int windowSize = 128;
 	public int windowOverlap = 10;
+	public int alphaCutoff = 8;
 	private const int SAMPLE_RATE = 256;
 	public enum EEG_CHANNEL: int {TP9=0, AF7=1, AF8=2, TP10=3};
 	public EEG_CHANNEL channelToRecordFrom = EEG_CHANNEL.TP9;
+	public enum COLOUR_SCHEME {BLUE_GREEN, YELLOW_RED};
+	private COLOUR_SCHEME currentScheme = COLOUR_SCHEME.BLUE_GREEN;
 	private ArrayList dataBuffer;
-	private int dataBufferLength;
+	private ArrayList baselineArray;
+	private double[] baselinePowerSpectrum;
 	private VRTK_InteractableObject interactableObject;
 
 	// Use this for initialization
@@ -27,7 +31,8 @@ public class ChangeFaceColour : MonoBehaviour {
 		material = GetComponent<Renderer> ().material;
 		texture = null;
 		dataBuffer = new ArrayList();
-		dataBufferLength = 0; 
+		baselineArray = new ArrayList ();
+		baselinePowerSpectrum = new double[windowSize];
 		interactableObject = GetComponentInParent<VRTK_InteractableObject> ();
 		interactableObject.InteractableObjectUngrabbed += new InteractableObjectEventHandler (StopRecordingData);
 	}
@@ -35,72 +40,84 @@ public class ChangeFaceColour : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		//Hold A to record EEG data
+		//Hold the object to record EEG data
 
-		if (Input.GetKey(KeyCode.Z) || interactableObject.IsGrabbed()) {
+		if (interactableObject.IsGrabbed()) {
 			RecordData ();
-		}
-
-		//Release A to convert EEG data to cube texture 
-		if (Input.GetKeyUp (KeyCode.Z)) {
-			StopRecording ();
 		}
 
 	}
 
-	void StopRecording() {
+	void ProcessBaselinePowerSpectrum() {
+		
+		double[] fullConvertedBuffer = baselineArray.ToArray().Select((object arg) => double.Parse(arg.ToString())).ToArray ();
+		ComplexSignal cs = Signal.FromArray (fullConvertedBuffer, SAMPLE_RATE).ToComplex ();
+		cs.ForwardFourierTransform ();
+		baselinePowerSpectrum = Tools.GetPowerSpectrum (cs.GetChannel (0));
+	}
 
-		Debug.Log ("stopped recording");
-        Debug.Log(dataBuffer.Count.ToString());
-		//Need to convert from ArrayList to double[] to use the Accord library 
-		double[] fullConvertedBuffer = dataBuffer.ToArray().Select((object arg) => double.Parse(arg.ToString())).ToArray ();
+	void ProcessData() {
+
+		double[] convertedBuffer = dataBuffer.ToArray().Select((object arg) => double.Parse(arg.ToString())).ToArray ();
 
 		//Data array length must be a power of 2 in order to convert to a Signal
-		int maxLength = (int)Math.Pow(2, (Math.Floor (Math.Log (fullConvertedBuffer.Length, 2.0))));
+		//int maxLength = (int)Math.Pow(2, (Math.Floor (Math.Log (fullConvertedBuffer.Length, 2.0))));
 
-		//Need to have enough data to be able to get the full power spectrum 
-		if (maxLength >= windowSize) {
+		//double[] convertedBuffer = new double[maxLength];
+		//Array.ConstrainedCopy (fullConvertedBuffer, 0, convertedBuffer, 0, maxLength);
 
+		//This array will hold the power spectrum values which will later be converted to colours 
+		double[] spectrogramMatrix = new double[(int) convertedBuffer.Length / windowOverlap * numFreqs];
 
-			double[] convertedBuffer = new double[maxLength];
-			Array.ConstrainedCopy (fullConvertedBuffer, 0, convertedBuffer, 0, maxLength);
+		int windowCount = 0;
+		for (int i = 0; i < convertedBuffer.Length - windowSize; i += windowOverlap) {
 
-			ComplexSignal cs = Signal.FromArray (convertedBuffer, SAMPLE_RATE).ToComplex ();
-
-			//This array will hold the power spectrum values which will later be converted to colours 
-			double[] spectrogramMatrix = new double[(int) convertedBuffer.Length / windowOverlap * numFreqs];
-
-			int windowCount = 0;
-			for (int i = 0; i < convertedBuffer.Length - windowSize; i += windowOverlap) {
-
-				//New array to hold just the data points for this time window 
-				double[] timeWindowArray = new double[windowSize];
-				Array.ConstrainedCopy (convertedBuffer, i, timeWindowArray, 0, windowSize);
+			//New array to hold just the data points for this time window 
+			double[] timeWindowArray = new double[windowSize];
+			Array.ConstrainedCopy (convertedBuffer, i, timeWindowArray, 0, windowSize);
 
 
-				cs = Signal.FromArray (timeWindowArray, SAMPLE_RATE).ToComplex ();
-				cs.ForwardFourierTransform ();
-				double[] powerSpectrum = Tools.GetPowerSpectrum (cs.GetChannel (0));
+			ComplexSignal cs = Signal.FromArray (timeWindowArray, SAMPLE_RATE).ToComplex ();
+			cs.ForwardFourierTransform ();
+			double[] powerSpectrum = Tools.GetPowerSpectrum (cs.GetChannel (0));
 
-				//Normalize the values 
-				double minPowerSpectrum = (powerSpectrum.Min ());
-				double maxPowerSpectrum = (powerSpectrum.Max ());
-				powerSpectrum = powerSpectrum.Select ((double arg) => (arg - minPowerSpectrum) / (maxPowerSpectrum - minPowerSpectrum)).ToArray ();
+			//Convert to dB
+			powerSpectrum = powerSpectrum.Select ((double arg, int index) => (10 * Math.Log10 (arg / baselinePowerSpectrum[index]))).ToArray ();
 
-				Array.ConstrainedCopy (powerSpectrum, 0, spectrogramMatrix, windowCount * numFreqs, numFreqs);
-				windowCount++;
-			}
+			// Normalzie values
+			double minPowerSpectrum = (powerSpectrum.Min ());
+			double maxPowerSpectrum = (powerSpectrum.Max ());
+			powerSpectrum = powerSpectrum.Select ((double arg, int index) => (arg - minPowerSpectrum) / (maxPowerSpectrum - minPowerSpectrum)).ToArray ();
 
-			Color[] spectrogramColours = spectrogramMatrix.Select (((double arg) => ColourUtility.HSL2RGB(arg, 0.75, 0.5))).ToArray ();
 
-			//Set the texture 
-			texture = new Texture2D ((int)convertedBuffer.Length / windowSize, numFreqs);
-			texture.SetPixels (spectrogramColours);
-            //material.mainTexture = null;
-			material.SetTexture ("_MainTex", texture);
-            dataBuffer = new ArrayList();
+			int highestFrequency = Array.IndexOf (powerSpectrum, powerSpectrum.Max ());
+			Debug.Log (highestFrequency.ToString ());
+			currentScheme = highestFrequency >= alphaCutoff ? COLOUR_SCHEME.YELLOW_RED : COLOUR_SCHEME.BLUE_GREEN;
+			Array.ConstrainedCopy (powerSpectrum, 0, spectrogramMatrix, windowCount * numFreqs, numFreqs);
+			windowCount++;
 		}
 
+		Color[] spectrogramColours;
+			
+		if (currentScheme == COLOUR_SCHEME.BLUE_GREEN) {
+			spectrogramColours = spectrogramMatrix.Select (((double arg) => ColourUtility.HSL2BlueGreen(arg, 0.75, 0.5))).ToArray ();
+		} else {
+			spectrogramColours = spectrogramMatrix.Select (((double arg) => ColourUtility.HSL2YellowRed (arg, 0.75, 0.5))).ToArray ();
+
+		}
+		//Set the texture 
+		texture = new Texture2D ((int)convertedBuffer.Length / windowSize, numFreqs);
+		texture.SetPixels (spectrogramColours);
+		//material.mainTexture = null;
+		material.SetTexture ("_MainTex", texture);
+
+
+		
+	}
+
+	void StopRecording() {
+
+         dataBuffer = new ArrayList();
 
 	}
 
@@ -111,9 +128,19 @@ public class ChangeFaceColour : MonoBehaviour {
 
 
 	void RecordData() {
-		Debug.Log ("recording");
-        Debug.Log(dataBuffer.Count.ToString());
+
         dataBuffer.Add (EEGData.eegData[(int)channelToRecordFrom]);
+
+		if (baselineArray.Count < windowSize) {
+			baselineArray.Add (EEGData.eegData[(int)channelToRecordFrom]);
+		}
+		if (baselineArray.Count == windowSize) {
+			ProcessBaselinePowerSpectrum ();
+		}
+
+		if (dataBuffer.Count > windowSize) {
+			ProcessData ();
+		}
 
 	}
 }
