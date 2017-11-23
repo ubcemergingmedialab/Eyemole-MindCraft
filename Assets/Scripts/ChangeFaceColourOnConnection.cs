9abd8839df9f37d9c99f0d4963356c3a856dfb43
ubceml <32344746+ubceml@users.noreply.github.com>
2017-11-22 18:55:53 -0800

    dynamic cubes hopefully

---
+++ Assets/Scripts/ChangeFaceColourOnConnection.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class ChangeFaceColourOnConnection : MonoBehaviour {
+
+    private Material material;
+    private const int SAMPLE_RATE = 256;
+    private VRTK_InteractableObject interactableObject;
+    private double lastEEGreceived;
+    private double currentEEGreceived;
+
+    // Use this for initialization
+    void Start () {
+        material = GetComponent<Renderer>().material;
+        interactableObject = GetComponentInParent<VRTK_InteractableObject>();
+        lastEEGreceived = 0;
+        currentEEGreceived = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (interactableObject.IsGrabbed()) {
+            RecordData();
+        }
+    }
+
+    void RecordData() {
+        lastEEGreceived = currentEEGreceived;
+        currentEEGreceived = EEGData.eegData[1];
+        if (currentEEGreceived != lastEEGreceived) {
+            Color currentColour = material.color;
+            Color newColour = new Color(1.0f - currentColour.r, 1.0f - currentColour.g, 1.0f - currentColour.b);
+            material.color = Color.Lerp(currentColour, newColour, 0.1f);
+        }
+    }
+}
