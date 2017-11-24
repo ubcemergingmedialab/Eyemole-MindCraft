d05d4a4cd79d0b8ee3a639b702e324ada8971bdf
Yana Pertels <ypertels@gmail.com>
2017-11-24 13:49:53 -0800

    working on save

---
+++ Assets/Scripts/CubeStateManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeStateManager : MonoBehaviour {
+
+	public static double[] baselineEEG;
+
+	// Use this for initialization
+	void Start () {
+
+		baselineEEG = new double[4];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void SaveFunction() {
+		
+	}
+
+	[Serializable]
+	public class PersonalityCube {
+		public double[] baselineEEG;
+		public List<PersonalityCubeFace> faces;
+
+	}
+
+	[Serializable]
+	public class PersonalityCubeFace{
+		public float positionX, positionY, positionZ;
+		public float rotationX, rotationY, rotationZ, rotationW;
+		public Texture2D texture;
+	}
+
+}
