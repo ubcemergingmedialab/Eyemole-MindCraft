92f628f7b3c6dbfa47041d1f94f37fa491da9776
asiaschmok <asiaschmok@gmail.com>
2017-11-24 14:25:50 -0800

    first commit

---
+++ vrtk demo/Assets/scripts/CreateObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateObject : MonoBehaviour {
+
+	public Transform Spawnpoint;
+	public Rigidbody Prefab;
+
+	void OnTriggerEnter () {
+		Rigidbody RigidPrefab;
+		RigidPrefab = Instantiate (Prefab, Spawnpoint.position, Spawnpoint.rotation) as Rigidbody;
+	}
+	
+
+}
