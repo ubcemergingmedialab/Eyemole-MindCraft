5fa57230d09a997c67f5d3cf81866af1bb42e94c
UBC EML <emergingmedia.lab@ubc.ca>
2017-12-18 17:01:25 -0800

    leap support for start scene

--- Assets/Scripts/ChangeSceneOnGrab.cs
+++ Assets/Scripts/ChangeSceneOnGrab.cs
@@ -3,21 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using VRTK;
+using Leap.Unity.Interaction;
 
 public class ChangeSceneOnGrab : MonoBehaviour {
 
 	public int sceneToChangeTo = 1;
 	private VRTK_InteractableObject interactableObject;
+	private InteractionBehaviour intObject;
 
 	// Use this for initialization
 	void Start () {
 		interactableObject = GetComponentInParent<VRTK_InteractableObject> ();
 		interactableObject.InteractableObjectGrabbed += new InteractableObjectEventHandler (ChangeScene);
+
+		intObject = GetComponentInParent<InteractionBehaviour>();
+		intObject.OnContactBegin += ChangeScene;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void ChangeScene() {
+		SceneManager.LoadScene(sceneToChangeTo);
 	}
 
 	void ChangeScene(object sender, InteractableObjectEventArgs e){
